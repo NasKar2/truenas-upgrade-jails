3b09f169c7f4adde04392acfbb36bbb0abc4084f
root <root@truenas.local>
2020-12-26 13:23:11 -0500

    prior changes were in temp file now copied to upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Fetch the current truenas release for the iocage jails and Upgrade your jails to the current truenas release
+# Fetch the current truenas release for the iocage jails and Upgrade your jails to the current truenas release or Update && Upgrade your jails
 # git clone https://github.com/NasKar2/truenas-upgrade-jails
-
+  
 print_msg () {
   echo -e "\e[1;32m"$1"\e[0m"
   echo
@@ -12,14 +12,24 @@ print_err () {
   echo
 }
 
+print_y () {
+  echo -e "\e[1;33m"$1"\e[0m"
+  echo
+}
+
+print_c () {
+  echo -e "\e[1;36m"$1"\e[0m"
+  echo
+}
+
 # Initialize Variables
 #
 SKIP_JAILS="duplicati urbackup"
 
 # Check if upgrade-config exists and read the file
-if [ -e "upgrade-config" ]; then 
   SCRIPT=$(readlink -f "$0")
   SCRIPTPATH=$(dirname "$SCRIPT")
+if [ -e "${SCRIPTPATH}/upgrade-config" ]; then
   . $SCRIPTPATH/upgrade-config
   print_msg "upgrade-config exists will read it"
 else
@@ -34,50 +44,29 @@ RELEASE=$(freebsd-version | cut -d - -f -1)"-RELEASE"
 
 if [ -z $POOL_PATH ]; then
   POOL_PATH="/mnt/$(iocage get -p)"
-  print_msg "POOL_PATH is defaulting to ${POOL_PATH}"                                                                
+  print_msg "POOL_PATH is defaulting to ${POOL_PATH}"
 fi
 
-#
-# Fetch
-#
-CURRENT_RELEASE=$(ls -Ar /mnt/v1/iocage/releases | head -n 1)
-#CURRENT_RELEASE=$(ls -Art /mnt/v1/iocage/releases | tail -n 1)
-print_msg "The most recent release available is ${CURRENT_RELEASE}"
-if [[ $# = 0 ]]; then
-   if ! [ $CURRENT_RELEASE = $RELEASE ]; then
-     print_msg "Fetching release ${RELEASE}"
-     iocage fetch -r $RELEASE
-   else
-     print_msg "Release ${CURRENT_RELEASE} already exists"
-   fi
-elif ! [[ $# = 0 ]] && ! [ $1 = "test" ]; then
-   print_err "Argument passed only test is a valid argument"
-   exit 1
-else
-   print_msg "Run in test mode"
-fi
+# Create array for jails
 
-#
-# Upgrade all jails
-#
 echo
 delete=(${SKIP_JAILS})
 cd ${POOL_PATH}/iocage/jails
 shopt -s dotglob
 shopt -s nullglob
-#array=($(ls -d ${POOL_PATH}/iocage/jails/* | cut -d '/' -f 6))
 array=($(ls -A ${POOL_PATH}/iocage/jails))
-#for jail in "${array[@]}"; do
-#print_msg "$jail"
-#done
 for target in "${delete[@]}"; do
   for i in "${!array[@]}"; do
     if [[ ${array[i]} = $target ]]; then
       unset 'array[i]'
     fi
   done
-done
+done                                                                                 
 echo "*******************"
+
+
+# Bash Menu
+
 PS3='Please enter your choice: '
 options=("Upgrade Jail Release" "Update && Upgrade" "Test Release Upgrade" "Quit")
 select opt in "${options[@]}"
@@ -103,45 +92,76 @@ do
         *) echo "invalid option $REPLY";;
     esac
 done
-if [ $opt = "Upgrade Jail Release" ]; then
-for jail in "${array[@]}"; do
-   echo "execute $opt"
-   exit 1
-CURRENT_RELEASE=$(iocage get release ${jail} | cut -d - -f -1)"-RELEASE"
- if [  "$CURRENT_RELEASE" = "$RELEASE" ]; then
-   print_err "The ${jail} is already upgraded to the current release ${RELEASE}" 
- else
-  if [ $(iocage get -s ${jail} ) = "down" ]; then                   
-     print_err "The jail named ${jail} is down please start it if you want to upgrade it"
-  elif [ $(iocage get -s ${jail} ) = "up" ]; then  
-     print_msg "Will upgrade ${jail} from ${CURRENT_RELEASE} to ${RELEASE}"
-          iocage upgrade -r $RELEASE $jail
-          iocage restart $jail
-          iocage exec $jail "pkg-static install -f -y pkg" # fix shared object 'libarchive.so.6' not found, required by 'pkg'
-          iocage exec $jail "pkg-static upgrade -f -y"
-          iocage restart $jail
-  fi
- fi
-done
-elif [ $opt = "Update && Upgrade" ]; then
-for jail in "${array[@]}"; do
-   echo "execute $opt"
-done
-elif [ $opt = "Test Release Upgrade" ]; then
-for jail in "${array[@]}"; do
-   echo "execute $opt
-CURRENT_RELEASE=$(iocage get release ${jail} | cut -d - -f -1)"-RELEASE"
- if [  "$CURRENT_RELEASE" = "$RELEASE" ]; then
-   print_err "The ${jail} is already upgraded to the current release ${RELEASE}"
-done
- else
-  if [ $(iocage get -s ${jail} ) = "down" ]; then
-     print_err "The jail named ${jail} is down please start it if you want to upgrade it"
-  elif [ $(iocage get -s ${jail} ) = "up" ]; then
-     print_msg "Will upgrade ${jail} from ${CURRENT_RELEASE} to ${RELEASE}"
-  fi
- fi
-done
-elif [ $opt = "Quit" ]; then
-echo "Quit"
+#print_msg "$opt"
+if [ "$opt" = "Upgrade Jail Release" ];then
+   print_msg "Upgrade Jail Release"
+   # Fetch
+   #
+   CURRENT_RELEASE=$(ls -Ar /mnt/v1/iocage/releases | head -n 1)
+   print_y "The most recent release available is ${CURRENT_RELEASE}"
+      if ! [ $CURRENT_RELEASE = $RELEASE ]; then
+        print_msg "Fetching release ${RELEASE}"
+        iocage fetch -r $RELEASE
+
+      else
+        print_y "Release ${CURRENT_RELEASE} already exists"
+      fi
+  for jail in "${array[@]}"; do
+      #echo "execute $opt"
+      CURRENT_RELEASE=$(iocage get release ${jail} | cut -d - -f -1)"-RELEASE"
+    if [  "$CURRENT_RELEASE" = "$RELEASE" ]; then
+      print_y "The ${jail} is already upgraded to the current release ${RELEASE}"
+    else
+      if [ $(iocage get -s ${jail} ) = "down" ]; then
+        print_err "The jail named ${jail} is down please start it if you want to upgrade it"
+      elif [ $(iocage get -s ${jail} ) = "up" ]; then
+        print_msg "Will upgrade ${jail} from ${CURRENT_RELEASE} to ${RELEASE}"
+        iocage upgrade -r $RELEASE $jail
+        iocage restart $jail
+        iocage exec $jail "pkg-static install -f -y pkg" # fix shared object 'libarchive.so.6' not found, required by 'pkg'
+        iocage exec $jail "pkg-static upgrade -f -y"
+        iocage restart $jail
+      fi
+    fi
+  done
+
+elif [ "$opt" = "Update && Upgrade" ];then
+      print_msg "Update && Upgrade"
+  for jail in "${array[@]}"; do
+      print_y "execute $opt for $jail"
+      iocage exec $jail "pkg update && pkg upgrade -y"
+  done
+elif [ "$opt" = "Test Release Upgrade" ];then
+      print_c "Test Release Upgrade"
+      # Fetch
+      #
+      CURRENT_RELEASE=$(ls -Ar /mnt/v1/iocage/releases | head -n 1)
+      print_c "The most recent release available is ${CURRENT_RELEASE}"
+      if ! [ $CURRENT_RELEASE = $RELEASE ]; then
+        print_c "Fetching release ${RELEASE}"
+        iocage fetch -r $RELEASE
+
+      else
+        print_c "Release ${CURRENT_RELEASE} already exists"
+      fi
+   for jail in "${array[@]}"; do
+      CURRENT_RELEASE=$(iocage get release ${jail} | cut -d - -f -1)"-RELEASE"
+      if [  "$CURRENT_RELEASE" = "$RELEASE" ]; then
+        print_c "The ${jail} is already upgraded to the current release ${RELEASE}" 
+      else
+        if [ $(iocage get -s ${jail} ) = "down" ]; then                   
+           print_err "The jail named ${jail} is down please start it if you want to upgrade it"
+        elif [ $(iocage get -s ${jail} ) = "up" ]; then  
+           print_c "Will upgrade ${jail} from ${CURRENT_RELEASE} to ${RELEASE}"
+          echo "iocage upgrade -r $RELEASE $jail"
+          echo "iocage restart $jail"
+          echo "iocage exec $jail "pkg-static install -f -y pkg" # fix shared object 'libarchive.so.6' not found, required by 'pkg'"
+          echo "iocage exec $jail "pkg-static upgrade -f -y""
+          echo "iocage restart $jail"
+        fi
+      fi
+  done
+elif [ "$opt" = "Quit" ];then
+   print_msg "Quit"
 fi
+
